528ab21c2441b99ce0a557b6f512b09b6d27e121
Dmytro Severniuk <d.severniuk@gmail.com>
2024-04-03 14:23:32 +0300

    feat: add catalog create validations

--- src/catalog/catalog.service.ts
+++ src/catalog/catalog.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateCatalogDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -6,11 +6,19 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class CatalogService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(catalog: CreateCatalogDto, userId: string) {
+  async create(catalog: CreateCatalogDto, userId: string) {
+    await this.checkNameIsUnique(catalog.name);
+
+    const primary = catalog.primary !== undefined ? catalog.primary : false;
+
+    if (primary) {
+      await this.checkOnePrimaryPerVertical(userId, catalog.vertical);
+    }
+
     const catalogToCreate = {
       ...structuredClone(catalog),
       user: { connect: { id: userId } },
-      primary: catalog.primary !== undefined ? catalog.primary : false,
+      primary,
     };
 
     return this.prismaService.catalog.create({ data: catalogToCreate });
@@ -31,4 +39,21 @@ export class CatalogService {
   // remove(id: number) {
   //   return `This action removes a #${id} catalog`;
   // }
+
+  private async checkNameIsUnique(name) {
+    const exitingCatalogForName = await this.prismaService.catalog.findUnique({ where: { name } });
+
+    if (exitingCatalogForName) {
+      throw new BadRequestException(`catalog with the name '${name}' is already exists`);
+    }
+  }
+
+  private async checkOnePrimaryPerVertical(userId: string, vertical: string) {
+    const existingUserCatalogsPerVertical =
+      (await this.prismaService.catalog.findMany({ where: { userId, vertical, primary: true } })) || [];
+
+    if (existingUserCatalogsPerVertical.length) {
+      throw new BadRequestException(`user may have only one primary catalog per vertical`);
+    }
+  }
 }
